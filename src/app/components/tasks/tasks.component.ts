daf9758b0b2c796f3c203efe2e3ac85cc1b5e26d
vinandros <vinandros@gmail.com>
2021-07-25 16:48:40 -0600

    Observables aproach

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -12,6 +12,8 @@ export class TasksComponent implements OnInit {
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
-    this.tasks = this.taskService.getTasks();
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
   }
 }
